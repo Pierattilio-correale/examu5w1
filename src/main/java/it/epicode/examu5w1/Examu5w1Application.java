1cb34e1c0410ca14010476ba531dfb314916ffbd
Pierattilio-correale <pierattiliocorreale@yahoo.it>
2025-06-06 10:10:30 +0200

    Initial commit

---
+++ src/main/java/it/epicode/examu5w1/Examu5w1Application.java
@@ -0,0 +1,13 @@
+package it.epicode.examu5w1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Examu5w1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Examu5w1Application.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/examu5w1/Examu5w1ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.examu5w1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Examu5w1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
